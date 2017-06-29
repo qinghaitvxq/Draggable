c2c67a27ea768b3de279d7eadf7b99f13dbe33eb
qinghaitvxq <598616202@qq.com>
2017-06-29 18:07:46 +0800

    add drag func

---
+++ EventTarget.js
@@ -0,0 +1,37 @@
+/**
+ * Created by caipf on 2017/6/29.
+ */
+function EventTarget(){
+    this.handlers={};
+}
+
+EventTarget.prototype={
+    addHandler:function(type,handler){
+        if(typeof this.handlers[type]=="undefined"){
+            this.handlers[type]=[];
+        }
+        this.handlers[type].push(handler);
+    },
+    fire:function(event){
+        if(!event.target){
+            event.target=this;
+        }
+        if(this.handlers[event.type] instanceof Array){
+            var handlers=this.handlers[event.type];
+            for(var i=0;i<handlers.length;i++){
+                handlers[i](event);
+            }
+        }
+    },
+    removeHandler:function(type,handler){
+        if(this.handlers[type] instanceof Array){
+            var handlers=this.handlers[type];
+            for(var i=0;i<handlers.length;i++){
+                if(handler[i]===handler){
+                    break;
+                }
+            }
+            handlers.splice(i,1);
+        }
+    }
+};
